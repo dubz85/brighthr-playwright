b34d07614b18d626bb9318bb05ef653787dd7697
Ismail <ishe1985@googlemail.com>
2025-10-06 13:42:29 +0100

    Update launch status to true

--- Scripts/save-auth-state.mjs
+++ Scripts/save-auth-state.mjs
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { chromium } from "playwright";
 
 (async () => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
 
